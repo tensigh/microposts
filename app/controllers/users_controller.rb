75726bc551b75413d407ff871e455a62b71d8c9b
tensigh <kurisu@linuxdiaries.com>
2015-12-05 06:33:22 +0000

    Amost ready for final submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authorized_access!, only: [:edit, :update]
+
+  def edit
+  end
+
+def update
+  if @user.update(user_params)
+    redirect_to @user
+  else
+     # unvalidated
+    render 'edit'
+  end
+end
 
   def show # 追加
    @user = User.find(params[:id])
@@ -19,9 +33,20 @@ class UsersController < ApplicationController
   end
 
   private
-
+  
+  def set_user
+       @user = User.find(params[:id])
+  end  
+  
   def user_params
-    params.require(:user).permit(:name, :screen_name, :phone_number, :region, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, :region, :phone_number, :screen_name)
   end
+  
+  def authorized_access!
+    if @user != current_user
+      redirect_to root_path, alert: 'Unauthorized access!'
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :screen_name, length: { maximum: 50 }
+  validates :screen_name, presence: true, length: { maximum: 50 }, on: :update
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :region, presence: true, length: { maximum: 20 }
-  validates :phone_number, presence: true, format: { with: /\d{3,4}-\d{4}-\d{4}/, message: "bad format" }, length: {maximum: 20}
+  validates :region, presence: true, length: { maximum: 20 }, on: :update
+  validates :phone_number, presence: true, format: { with: /\A\+?\d[-\d\s]+\Z/, message: "Use only digits and dashes for phone number" }, length: {maximum: 20}, on: :update
   has_secure_password
 end
