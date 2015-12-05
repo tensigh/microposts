59f536a7d38c249713154b447e937f430630bd3d
tensigh <kurisu@linuxdiaries.com>
2015-12-05 07:18:52 +0000

    Final version, all functions work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+def index
+  @users = User.all
+end
+
 def update
   if @user.update(user_params)
     redirect_to @user
@@ -14,39 +18,39 @@ def update
   end
 end
 
-  def show # 追加
-   @user = User.find(params[:id])
-  end
+def show # 追加
+  @user = User.find(params[:id])
+end
   
-  def new
-    @user = User.new
-  end
+def new
+  @user = User.new
+end
 
-  def create
-    @user = User.new(user_params)
+def create
+  @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
-  end
+end
 
-  private
+private
   
-  def set_user
-       @user = User.find(params[:id])
-  end  
+def set_user
+  @user = User.find(params[:id])
+end  
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
+def user_params
+  params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :region, :phone_number, :screen_name)
-  end
+end
   
-  def authorized_access!
-    if @user != current_user
+def authorized_access!
+  if @user != current_user
       redirect_to root_path, alert: 'Unauthorized access!'
-    end
   end
-  
+end
+
 end
