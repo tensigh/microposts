2b729cf39ceb7f81de3f552cf05a7059e9a535fb
tensigh <kurisu@linuxdiaries.com>
2015-12-12 05:31:18 +0000

    Finally got it all to work, thanks to Kano-san!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
   before_action :authorized_access!, only: [:edit, :update]
+
+  def followings
+    @user = User.find(params[:id])
+    @followings =@user.following_users.order(created_at: :desc)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers =@user.follower_users.order(created_at: :desc) 
+  end
 
   def show # 追加
    @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
-   @users = @user.following_users
   end
 
   def index
-    @users = User.all
+   @users = User.all
   end
 
   def update
@@ -26,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+   @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -42,7 +51,7 @@ class UsersController < ApplicationController
   end  
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+   params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :region, :phone_number, :screen_name)
   end
   
