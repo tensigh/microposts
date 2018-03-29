ee082cc06d7ee3e1cc753a8035f322d09e5a2395
tensigh <kurisu@linuxdiaries.com>
2018-03-29 02:42:20 +0000

    Includes fix for form

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,25 @@
 class StoreController < ApplicationController
-    def new
-        @store = Store.new
+  def index
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to root_path
+      flash[:notice] = '店舗追加が出来ました'
+    else
+       @store = Store.all
+       flash.now[:alert] = "メッセージの保存に失敗しました。"
+       render 'index'
     end
+  end
+
+private
+  def store_params
+    params.require(:store).permit(:restaurantName, :restaurantAdd, :restaurantPhone, :restaurantWebsite, :productName, :productPrice, :productRating, :comments)
+  end
+end
+  
+  
 end
